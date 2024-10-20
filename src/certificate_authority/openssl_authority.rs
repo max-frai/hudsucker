5b484f07caa29b930c6837c0bafd5cfa8fdcf86e
Max Frai <me@maxfrai.com>
2024-10-20 19:27:16 +0200

    Fix openssl authority to support all needed extensions

--- src/certificate_authority/openssl_authority.rs
+++ src/certificate_authority/openssl_authority.rs
@@ -8,7 +8,14 @@ use openssl::{
     hash::MessageDigest,
     pkey::{PKey, Private},
     rand,
-    x509::{extension::SubjectAlternativeName, X509Builder, X509NameBuilder, X509},
+    rsa::Rsa,
+    x509::{
+        extension::{
+            AuthorityKeyIdentifier, BasicConstraints, ExtendedKeyUsage, KeyUsage,
+            SubjectAlternativeName, SubjectKeyIdentifier,
+        },
+        X509Builder, X509NameBuilder, X509,
+    },
 };
 use std::{
     sync::Arc,
@@ -19,40 +26,10 @@ use tokio_rustls::rustls::{
     pki_types::{CertificateDer, PrivateKeyDer, PrivatePkcs8KeyDer},
     ServerConfig,
 };
-use tracing::debug;
+use tracing::{debug, error};
 
-/// Issues certificates for use when communicating with clients.
-///
-/// Issues certificates for communicating with clients over TLS. Certificates are cached in memory
-/// up to a max size that is provided when creating the authority. Certificates are generated using
-/// the `openssl` crate.
-///
-/// # Examples
-///
-/// ```rust
-/// use hudsucker::{
-///     certificate_authority::OpensslAuthority,
-///     openssl::{hash::MessageDigest, pkey::PKey, x509::X509},
-///     rustls::crypto::aws_lc_rs,
-/// };
-///
-/// let private_key_bytes: &[u8] = include_bytes!("../../examples/ca/hudsucker.key");
-/// let ca_cert_bytes: &[u8] = include_bytes!("../../examples/ca/hudsucker.cer");
-/// let private_key = PKey::private_key_from_pem(private_key_bytes).unwrap();
-/// let ca_cert = X509::from_pem(ca_cert_bytes).unwrap();
-///
-/// let ca = OpensslAuthority::new(
-///     private_key,
-///     ca_cert,
-///     MessageDigest::sha256(),
-///     1_000,
-///     aws_lc_rs::default_provider(),
-/// );
-/// ```
-#[cfg_attr(docsrs, doc(cfg(feature = "openssl-ca")))]
 pub struct OpensslAuthority {
     pkey: PKey<Private>,
-    private_key: PrivateKeyDer<'static>,
     ca_cert: X509,
     hash: MessageDigest,
     cache: Cache<Authority, Arc<ServerConfig>>,
@@ -60,7 +37,6 @@ pub struct OpensslAuthority {
 }
 
 impl OpensslAuthority {
-    /// Creates a new openssl authority.
     pub fn new(
         pkey: PKey<Private>,
         ca_cert: X509,
@@ -68,14 +44,8 @@ impl OpensslAuthority {
         cache_size: u64,
         provider: CryptoProvider,
     ) -> Self {
-        let private_key = PrivateKeyDer::from(PrivatePkcs8KeyDer::from(
-            pkey.private_key_to_pkcs8()
-                .expect("Failed to encode private key"),
-        ));
-
         Self {
             pkey,
-            private_key,
             ca_cert,
             hash,
             cache: Cache::builder()
@@ -86,7 +56,15 @@ impl OpensslAuthority {
         }
     }
 
-    fn gen_cert(&self, authority: &Authority) -> Result<CertificateDer<'static>, ErrorStack> {
+    fn gen_cert(
+        &self,
+        authority: &Authority,
+    ) -> Result<(CertificateDer<'static>, PrivateKeyDer<'static>), ErrorStack> {
+        // Generate a new RSA key pair for the leaf certificate
+        let rsa = Rsa::generate(2048)?;
+        let keypair = PKey::from_rsa(rsa)?;
+
+        // Build the subject name with the authority host
         let mut name_builder = X509NameBuilder::new()?;
         name_builder.append_entry_by_text("CN", authority.host())?;
         let name = name_builder.build();
@@ -95,6 +73,7 @@ impl OpensslAuthority {
         x509_builder.set_subject_name(&name)?;
         x509_builder.set_version(2)?;
 
+        // Set the validity period
         let not_before = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .expect("Failed to determine current UNIX time")
@@ -103,24 +82,60 @@ impl OpensslAuthority {
         x509_builder.set_not_before(Asn1Time::from_unix(not_before)?.as_ref())?;
         x509_builder.set_not_after(Asn1Time::from_unix(not_before + TTL_SECS)?.as_ref())?;
 
-        x509_builder.set_pubkey(&self.pkey)?;
+        // Set the public key for the certificate (leaf's public key)
+        x509_builder.set_pubkey(&keypair)?;
         x509_builder.set_issuer_name(self.ca_cert.subject_name())?;
+
+        // Generate a random serial number
+        let mut serial_number = [0; 16];
+        rand::rand_bytes(&mut serial_number)?;
+        let serial_number = BigNum::from_slice(&serial_number)?;
+        let serial_number = Asn1Integer::from_bn(&serial_number)?;
+        x509_builder.set_serial_number(&serial_number)?;
+
+        // Add required extensions
+        let basic_constraints = BasicConstraints::new().critical().build()?;
+        x509_builder.append_extension(basic_constraints)?;
+
+        let key_usage = KeyUsage::new()
+            .critical()
+            .digital_signature()
+            .key_encipherment()
+            .build()?;
+        x509_builder.append_extension(key_usage)?;
+
+        let ext_key_usage = ExtendedKeyUsage::new().server_auth().build()?;
+        x509_builder.append_extension(ext_key_usage)?;
+
+        let subject_key_id = SubjectKeyIdentifier::new()
+            .build(&x509_builder.x509v3_context(Some(&self.ca_cert), None))?;
+        x509_builder.append_extension(subject_key_id)?;
+
+        let authority_key_id = AuthorityKeyIdentifier::new()
+            .keyid(true)
+            .build(&x509_builder.x509v3_context(Some(&self.ca_cert), None))?;
+        x509_builder.append_extension(authority_key_id)?;
 
         let alternative_name = SubjectAlternativeName::new()
             .dns(authority.host())
             .build(&x509_builder.x509v3_context(Some(&self.ca_cert), None))?;
         x509_builder.append_extension(alternative_name)?;
 
-        let mut serial_number = [0; 16];
-        rand::rand_bytes(&mut serial_number)?;
-
-        let serial_number = BigNum::from_slice(&serial_number)?;
-        let serial_number = Asn1Integer::from_bn(&serial_number)?;
-        x509_builder.set_serial_number(&serial_number)?;
-
+        // Sign the certificate with the CA's private key
         x509_builder.sign(&self.pkey, self.hash)?;
         let x509 = x509_builder.build();
-        Ok(CertificateDer::from(x509.to_der()?))
+
+        // Serialize the certificate and private key
+        let cert_der = x509.to_der()?;
+
+        // Get the private key in PKCS8 DER format
+        let private_key_der = keypair.private_key_to_pkcs8()?;
+        let private_key_der = PrivateKeyDer::from(PrivatePkcs8KeyDer::from(private_key_der));
+
+        Ok((
+            CertificateDer::from(cert_der),
+            PrivateKeyDer::from(private_key_der),
+        ))
     }
 }
 
@@ -132,15 +147,21 @@ impl CertificateAuthority for OpensslAuthority {
         }
         debug!("Generating server config");
 
-        let certs = vec![self
-            .gen_cert(authority)
-            .unwrap_or_else(|_| panic!("Failed to generate certificate for {}", authority))];
+        let (cert_der, private_key_der) = match self.gen_cert(authority) {
+            Ok((cert, key)) => (cert, key),
+            Err(e) => {
+                error!("Failed to generate certificate for {}: {}", authority, e);
+                panic!("Failed to generate certificate");
+            }
+        };
+
+        let certs = vec![cert_der];
 
         let mut server_cfg = ServerConfig::builder_with_provider(Arc::clone(&self.provider))
             .with_safe_default_protocol_versions()
             .expect("Failed to specify protocol versions")
             .with_no_client_auth()
-            .with_single_cert(certs, self.private_key.clone_key())
+            .with_single_cert(certs, private_key_der)
             .expect("Failed to build ServerConfig");
 
         server_cfg.alpn_protocols = vec![
@@ -156,53 +177,5 @@ impl CertificateAuthority for OpensslAuthority {
             .await;
 
         server_cfg
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use tokio_rustls::rustls::crypto::aws_lc_rs;
-
-    fn build_ca(cache_size: u64) -> OpensslAuthority {
-        let private_key_bytes: &[u8] = include_bytes!("../../examples/ca/hudsucker.key");
-        let ca_cert_bytes: &[u8] = include_bytes!("../../examples/ca/hudsucker.cer");
-        let private_key =
-            PKey::private_key_from_pem(private_key_bytes).expect("Failed to parse private key");
-        let ca_cert = X509::from_pem(ca_cert_bytes).expect("Failed to parse CA certificate");
-
-        OpensslAuthority::new(
-            private_key,
-            ca_cert,
-            MessageDigest::sha256(),
-            cache_size,
-            aws_lc_rs::default_provider(),
-        )
-    }
-
-    #[test]
-    fn unique_serial_numbers() {
-        let ca = build_ca(0);
-
-        let authority1 = Authority::from_static("example.com");
-        let authority2 = Authority::from_static("example2.com");
-
-        let c1 = ca.gen_cert(&authority1).unwrap();
-        let c2 = ca.gen_cert(&authority2).unwrap();
-        let c3 = ca.gen_cert(&authority1).unwrap();
-        let c4 = ca.gen_cert(&authority2).unwrap();
-
-        let (_, cert1) = x509_parser::parse_x509_certificate(&c1).unwrap();
-        let (_, cert2) = x509_parser::parse_x509_certificate(&c2).unwrap();
-
-        assert_ne!(cert1.raw_serial(), cert2.raw_serial());
-
-        let (_, cert3) = x509_parser::parse_x509_certificate(&c3).unwrap();
-        let (_, cert4) = x509_parser::parse_x509_certificate(&c4).unwrap();
-
-        assert_ne!(cert3.raw_serial(), cert4.raw_serial());
-
-        assert_ne!(cert1.raw_serial(), cert3.raw_serial());
-        assert_ne!(cert2.raw_serial(), cert4.raw_serial());
     }
 }
